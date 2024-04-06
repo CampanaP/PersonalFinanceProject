20123710bb0325742b4b8dc6605374be54ff6729
Pablo Campana <pablo.campana@midaticket.it>
2024-04-06 13:36:52 +0200

    Add TransactionCategoryUpdateValidator

---
+++ PersonalFinanceProject.Business.Transaction/Validators/TransactionCategory/TransactionCategoryUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PersonalFinanceProject.Business.Transaction.Messages.TransactionCategory.Requests;
+
+namespace PersonalFinanceProject.Business.Transaction.Validators.TransactionCategory
+{
+    public class TransactionCategoryUpdateValidator : AbstractValidator<TransactionCategoryUpdateRequestMessage>
+    {
+        public TransactionCategoryUpdateValidator()
+        {
+            RuleFor(tc => tc.Id).NotNull();
+            RuleFor(tc => tc.Id).NotEqual(default(int));
+
+            RuleFor(tc => tc.Name).NotNull();
+            RuleFor(tc => tc.Name).NotEmpty();
+            RuleFor(tc => tc.Name).NotEqual(" ");
+        }
+    }
+}
